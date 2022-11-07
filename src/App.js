cc2b78f2110abfbfaaa16f5c441caf3152719fb4
Kemunto-wendy <joy.kemunto@student.moringaschool.com>
2022-11-07 23:35:31 +0300

    confirmed my files are working

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import {Routes, Route} from "react-router-dom"
+import "./App.css"
 
 const App = ( ) => {
 return (
-  <>
+  <div>
     <Routes>
         <Route>
         </Route>
     </Routes>
-  </>
+  </div>
 )
 }
 
