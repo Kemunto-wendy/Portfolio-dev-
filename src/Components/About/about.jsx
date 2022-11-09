1caa4788aacd2e5e5c448b15b945b73314cffa76
Kemunto-wendy <joy.kemunto@student.moringaschool.com>
2022-11-09 17:11:27 +0300

    added assets to my documentation

--- src/Components/About/about.jsx
+++ src/Components/About/about.jsx
@@ -2,7 +2,15 @@ import React from "react";
 
 const About = ( ) => {
     return (
-        <div>
+        <div className="about">
+            <div className="bout">
+                <h1>Get to know me</h1>
+            </div>
+
+            <div className="button">
+            <a href={CV} download className="btn" >Download CV </a>
+            <a href={CV} view className="btn-primary">  View Resume </a>
+            </div>
         </div>
     )
 }
